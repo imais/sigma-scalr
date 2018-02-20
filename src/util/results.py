Fix critical mistake on mst std deviation computation (cause: incorrect initialization of self.ready flag in mst.py)

src/util/results.py
@@ -13,8 +13,11 @@ class Results(object):
 		
 	def print_stats(self):
 		violations = [1.0 if row['workload'] > row['mst_tru'] else 0 for index, row in self.df.iterrows()]
-		print('Stats: %.3f, %.3f, %.3f' % (100 * np.mean(violations), np.mean(self.df['backlog']),
-										   np.mean(self.df['m'])))
+		print('Stats: %.3f, %.3f, %.3f, %.3f' % 
+			  (100 * np.mean(violations), 
+			   np.mean(self.df['m']), 
+			   np.mean(self.df['backlog']),
+			   (np.mean(self.df['backlog']) / np.mean(self.df['workload']))))
 
 
 	def write_results(self, file):